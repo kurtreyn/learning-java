9405ae6ba1e6329e598c331e511ff83c93ba98eb
kurtreyn <kurt.reynolds@twinspires.com>
2023-06-15 16:37:06 -0400

    added AppClass & AppSubClass

---
+++ src/AppSubClass.java
@@ -0,0 +1,36 @@
+public class AppSubClass {
+    private String name;
+    public String email;
+
+    //    CONSTRUCTOR
+   public AppSubClass(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public AppSubClass() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void log(String message) {
+        System.out.println(message);
+    }
+
+
+}
