4866af176a514d789e2a4aa5060a3a101f4c2437
kurtreyn <kurt.reynolds@twinspires.com>
2023-07-21 10:11:15 -0400

    added Maps & ArraysAndArrayList

---
+++ src/Maps.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class Maps {
+    public static void main(String[] args) {
+        HashMap<String, Integer> employeeIds = new HashMap<>();
+        employeeIds.put("John", 123);
+        employeeIds.put("Jane", 456);
+        System.out.println("John's employee id is " + employeeIds.get("John"));
+        System.out.println("Jane's exists in map: " + employeeIds.containsKey("Jane"));
+        System.out.println("Jane's employee id is " + employeeIds.get("Jane"));
+        employeeIds.replace("Jane", 789);
+        System.out.println("Jane's employee id is now " + employeeIds.get("Jane"));
+        employeeIds.putIfAbsent("Steve", 123);
+        System.out.println("Steve's employee id is " + employeeIds.get("Steve"));
+    }
+}
